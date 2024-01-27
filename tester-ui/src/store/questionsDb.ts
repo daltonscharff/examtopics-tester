138319192f3ce009d9c3ebc9d71dfee6b8b931bc
Dalton Scharff <dalton.scharff@gmail.com>
2024-01-26 20:33:03 -0500

    create questions db

---
+++ tester-ui/src/store/questionsDb.ts
@@ -0,0 +1,23 @@
+import questions from "../assets/questions.json";
+import Dexie, { Table } from "dexie";
+
+type Question = {
+  id: string;
+  question: string;
+  choices: { text: string; letter: string }[];
+  answers: string[];
+};
+
+class QuestionsDatabase extends Dexie {
+  public questions!: Table<Question, string>;
+
+  public constructor() {
+    super("QuestionsDatabase");
+    this.version(1).stores({
+      questions: "++id,question,choices,answers",
+    });
+    this.questions.bulkAdd(questions);
+  }
+}
+
+export const db = new QuestionsDatabase();
