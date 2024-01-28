a6a1012e9d32a99fc6dc5a5af8f4c535040fc3e1
Dalton Scharff <dalton.scharff@gmail.com>
2024-01-28 15:33:11 -0500

    replace questionsDb with questionsStore

---
+++ tester-ui/src/store/questionsStore.ts
@@ -0,0 +1,42 @@
+import questions from "../assets/questions.json";
+import { create } from "zustand";
+import { shuffleArray } from "../utils";
+
+export type Question = {
+  id: string;
+  question: string;
+  choices: { text: string; letter: string }[];
+  answers: string[];
+};
+
+export type QuestionId = Question["id"];
+
+interface QuestionsStore {
+  size: number;
+  getById: (...questionIds: QuestionId[]) => Map<QuestionId, Question>;
+  getRandomQuestion: (quantity: number) => Question[];
+}
+
+const questionsMap = new Map<QuestionId, Question>();
+questions.forEach((question) => questionsMap.set(question.id, question));
+
+export const useQuestionsStore = create<QuestionsStore>(() => ({
+  size: questions.length,
+  getById: (...questionIds: QuestionId[]) => {
+    const q = new Map<QuestionId, Question>();
+    questionIds.forEach((id) => {
+      if (!questionsMap.has(id)) {
+        throw new Error(`question id not found: ${id}`);
+      }
+      q.set(id, questionsMap.get(id)!);
+    });
+    return q;
+  },
+  getRandomQuestion: (quantity: number = 1) => {
+    const maxLength = questions.length;
+    if (quantity >= maxLength) {
+      quantity = maxLength;
+    }
+    return shuffleArray(questions).slice(0, quantity);
+  },
+}));
