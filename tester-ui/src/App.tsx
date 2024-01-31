67e58fb3468b72e6d7c11932df5826fefc3ca375
Dalton Scharff <dalton.scharff@gmail.com>
2024-01-30 21:19:44 -0500

    add progress bar to measure percent correct

--- tester-ui/src/App.tsx
+++ tester-ui/src/App.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@nextui-org/react";
+import { Card, CardBody, Progress, cn } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 import { useQuestionsStore } from "./store/questionsStore";
+import { useAnswerStore } from "./store/answerStore";
 
 const QUIZ_LENGTH = 10;
 
@@ -12,19 +13,49 @@ function App() {
   }
 
   return (
-    <>
-      <p>{numOfQuestions} Questions!</p>
-      {quizStartingNumbers.map((start) => {
-        const end = start + QUIZ_LENGTH - 1;
-        return (
-          <Link to={`/quiz/${start}...${end}`} key={`/quiz/${start}...${end}`}>
-            <Button>
-              Questions {start} - {end}
-            </Button>
-          </Link>
-        );
-      })}
-    </>
+    <main className={cn("container mx-auto p-2 py-3 flex flex-col gap-4")}>
+      <div className="text-center">
+        <h1 className="text-xl">ExamTopics Tester</h1>
+        <h1 className="text-xl">AWS Certified Solutions Architect</h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 flex-wrap">
+        {quizStartingNumbers.map((start) => {
+          const getQuestions = useQuestionsStore((state) => state.getQuestions);
+          const getMostRecentAnswer = useAnswerStore(
+            (state) => state.getMostRecentAnswer
+          );
+          const end = start + QUIZ_LENGTH - 1;
+          const questions = getQuestions(start - 1, end - 1);
+          const percentCorrect =
+            (questions.reduce((total, question) => {
+              if (getMostRecentAnswer(question.id)?.isCorrect) {
+                return (total += 1);
+              }
+              return total;
+            }, 0) /
+              QUIZ_LENGTH) *
+            100;
+          return (
+            <Link
+              to={`/quiz/${start}...${end}`}
+              key={`/quiz/${start}...${end}`}
+            >
+              <Card className="rounded-sm hover:bg-zinc-50">
+                <CardBody>
+                  Questions #{start} - #{end}
+                </CardBody>
+                <Progress
+                  size="sm"
+                  color="success"
+                  aria-label="Percent correct"
+                  value={percentCorrect}
+                />
+              </Card>
+            </Link>
+          );
+        })}
+      </div>
+    </main>
   );
 }
 
